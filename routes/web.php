c318e1245b7c1567107c01d968fe9237d52b7a7c
Jorge Luis Pinto Velandia <pintojorgeluis16@gmail.com>
2026-02-12 10:54:52 -0500

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mi-nombre', function () {
+    return "Jorge Luis Pinto";
+});
+
+Route::get('/php-basico', function () {
+    
+
+
+
+    return "Jorge Luis Pinto";
+});
